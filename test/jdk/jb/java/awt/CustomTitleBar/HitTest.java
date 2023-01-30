2779ddde7a76343540c4d5b60990ab9c6d647352
Sergey Shelomentsev <sergey.shelomentsev@jetbrains.com>
2023-01-30 21:05:35 +0200

    update hit test

--- test/jdk/jb/java/awt/CustomTitleBar/HitTest.java
+++ test/jdk/jb/java/awt/CustomTitleBar/HitTest.java
@@ -22,8 +22,10 @@ import util.TestUtils;
 import java.awt.AWTException;
 import java.awt.Button;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Panel;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
@@ -64,7 +66,6 @@ public class HitTest {
 
         @Override
         public void customizeWindow() {
-            System.out.println("custom");
             button = new Button();
             button.setBackground(Color.CYAN);
             button.setSize(80, 40);
@@ -75,9 +76,7 @@ public class HitTest {
 
                 @Override
                 public void mouseClicked(MouseEvent e) {
-                    System.out.println("e = " + e.getButton());
                     hit();
-                    System.out.println("ex = " + e.getButton());
                     if (e.getButton() == 1) {
                         gotButton1Click = true;
                     }
@@ -171,6 +170,9 @@ public class HitTest {
 
     private static final Runner hitTestClientArea = new Runner("Hit test client area") {
 
+        private Panel panel1;
+        private Panel panel2;
+
         private boolean gotMouseClick = false;
         @Override
         public void prepareTitleBar() {
@@ -179,33 +181,48 @@ public class HitTest {
         }
 
         @Override
-        public void test() throws AWTException {
-            Button button = new Button();
-            button.setBounds(200, 20, 80, 40);
-            button.addMouseListener(new MouseAdapter() {
-
-                private void hit() {
-                    titleBar.forceHitTest(true);
+        protected void customizeWindow() {
+            panel1 = new Panel(){
+                @Override
+                public void paint(Graphics g) {
+                    Rectangle r = g.getClipBounds();
+                    g.setColor(Color.BLUE);
+                    g.fillRect(r.x, r.y, 200, 100);
+                    super.paint(g);
                 }
-
+            };
+            panel1.setBounds(0, 0, 200, 100);
+            panel1.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
-                    hit();
-                    if (e.getButton() == 1) {
-                        gotMouseClick = true;
-                    }
+                    System.out.println("mouse clicked to panel1");
                 }
             });
-            window.add(button);
 
-            Robot robot = new Robot();
-            robot.delay(1000);
-            robot.mouseMove(button.getBounds().x + button.getBounds().width / 2, button.getBounds().y + button.getBounds().height / 2);
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            panel2 = new Panel(){
+                @Override
+                public void paint(Graphics g) {
+                    Rectangle r = g.getClipBounds();
+                    g.setColor(Color.RED);
+                    g.fillRect(r.x, r.y, 200, 100);
+                    super.paint(g);
+                }
+            };
+            panel2.setBounds(200, 0, 200, 100);
+            panel2.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    System.out.println("mouse clicked to panel2");
+                }
+            });
 
-            passed = gotMouseClick;
-            System.out.println("PASSED!");
+            window.add(panel1);
+            window.add(panel2);
+        }
+
+        @Override
+        public void test() throws AWTException {
+
         }
 
     };
